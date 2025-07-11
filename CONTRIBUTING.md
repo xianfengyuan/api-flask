87309920a4c1d097c23b85ed45a2fad229a9fbda
Xianfeng Yuan <xianyuan@gmail.com>
2025-07-10 20:28:12 -0700

    launch local db

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -9,5 +9,5 @@ docker-buildx build -t api-flask .
 ## How to run docker locally
 
 ```
-docker run -p 5005:5000 -w /app -v "$(pwd):/app" IMAGE_NAME sh -c "flask run --host 0.0.0.0"
+docker run -p 8080:5000 -w /app -v "$(pwd):/app" api-flask sh -c "flask run --host 0.0.0.0"
 ```
--- app.py
+++ app.py
@@ -10,9 +10,11 @@ from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 
 from db import db
+from dotenv import load_dotenv
 
 def create_app(db_url=None):
   app = Flask(__name__)
+  load_dotenv()
 
   app.config["PROPAGATE_EXCEPTIONS"] = True
   app.config["API_TITLE"] = "Stores REST API"
